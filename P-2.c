c3b17e511d3b14f48d6d516f4ec6857532813ddb
Ahana-2005 <145107720+Ahana-2005@users.noreply.github.com>
2023-11-28 20:19:03 +0530

    Update and rename p2 to P-2.c

--- P-2.c
+++ P-2.c
@@ -1,9 +1,9 @@
 //Write a program in c to print the following pattern
-        1
-      1 2
-    1 2 3
-  1 2 3 4
-1 2 3 4 5
+/*        1
+        1 2
+      1 2 3
+    1 2 3 4
+  1 2 3 4 5 */
 #include <stdio.h>
 int main()
 {
