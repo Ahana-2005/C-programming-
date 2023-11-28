e1fc566c90ecf59e3db785fa561813362383b142
Ahana-2005 <145107720+Ahana-2005@users.noreply.github.com>
2023-11-28 20:17:15 +0530

    Update and rename p11 to P-11.c

--- P-11.c
+++ P-11.c
@@ -2,14 +2,13 @@
 #include<stdio.h>
 int imp(int a)
 {
-   printf("%d",a);
+printf("%d",a);
 }
-
 int main()
 {
-   int a=3;
-   printf("%d\n",a);
-   printf("Enter Number: ");
-   scanf("%d", &a);
-   imp(a);
+int a=3;
+printf("%d\n",a);
+printf("Enter Number: ");
+scanf("%d", &a);
+imp(a);
 }
