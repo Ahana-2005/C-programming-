d67077b7d82148a61e19b094052d2aa63ca76d9a
Ahana-2005 <145107720+Ahana-2005@users.noreply.github.com>
2023-11-28 20:16:45 +0530

    Update and rename p10 to P-10.c

---
+++ P-10.c
@@ -0,0 +1,21 @@
+//Write a program in c to print fibonacci series using function.
+#include<stdio.h>
+void fib(int r)
+{
+int a=0, b=1, c;
+while (a<=r)
+{
+printf("%d\t", a);
+c = a+b;
+a = b;
+b = c;
+}
+}
+int main()
+{
+int r;
+printf("Enter range: ");
+scanf("%d", &r);
+int a=fib(r);
+printf("The fibonacci series is: %d\n",a);
+}
